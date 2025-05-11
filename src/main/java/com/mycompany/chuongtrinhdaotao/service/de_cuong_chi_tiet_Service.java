c9ed4b0413a0ff54e42da46e7ff47292b5a59b30
hatuongvy1451 <147377307+hatuongvy1451@users.noreply.github.com>
2025-05-11 18:12:28 +0700

    Update de_cuong_chi_tiet_Service.java

--- src/main/java/com/mycompany/chuongtrinhdaotao/service/de_cuong_chi_tiet_Service.java
+++ src/main/java/com/mycompany/chuongtrinhdaotao/service/de_cuong_chi_tiet_Service.java
@@ -28,43 +28,4 @@ public class de_cuong_chi_tiet_Service {
     public de_cuong_chi_tiet getDeCuongChiTietById(int id){
         return deCuongChiTietRepository.findById(id).orElse(null);
     }
-    
-    public boolean deleteDeCuongChiTietById(int id){
-        Optional<de_cuong_chi_tiet> optional = deCuongChiTietRepository.findById(id);
-        
-        if(optional.isPresent()){
-            de_cuong_chi_tiet dcct = optional.get();
-            
-            if(dcct.getTrangThai() == 1){
-                dcct.setTrangThai(0);
-                deCuongChiTietRepository.save(dcct);
-            } else {
-                deCuongChiTietRepository.deleteById(id);
-            }
-            
-            return true;
-        }
-        return false;
-    }
-    
-    public de_cuong_chi_tiet updateDeCuongChiTietById(int id, de_cuong_chi_tiet updated) {
-        Optional<de_cuong_chi_tiet> optional = deCuongChiTietRepository.findById(id);
-        if (optional.isPresent()) {
-            de_cuong_chi_tiet current = optional.get();
-            // Cập nhật các thuộc tính
-            current.setMoTa(updated.getMoTa());
-            current.setMucTieu(updated.getMucTieu());
-            current.setChuongNoiDung(updated.getChuongNoiDung());
-            current.setPhuongPhapDanhGia(updated.getPhuongPhapDanhGia());
-            current.setPhuongPhapGiangDay(updated.getPhuongPhapGiangDay());
-            current.setTaiLieuThamKhao(updated.getTaiLieuThamKhao());
-            current.setTrangThai(updated.getTrangThai());
-            return deCuongChiTietRepository.save(current);
-        }
-        return null;
-    }
-    
-    public de_cuong_chi_tiet addDeCuongChiTiet(de_cuong_chi_tiet deCuongChiTiet){
-        return deCuongChiTietRepository.save(deCuongChiTiet);
-    }
 }
