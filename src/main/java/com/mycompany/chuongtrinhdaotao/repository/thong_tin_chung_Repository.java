3cb1525cd4c7d0ddca33d269f85270801b7cae1e
hatuongvy1451 <147377307+hatuongvy1451@users.noreply.github.com>
2025-05-15 10:34:22 +0700

    thống kê, in

--- src/main/java/com/mycompany/chuongtrinhdaotao/repository/thong_tin_chung_Repository.java
+++ src/main/java/com/mycompany/chuongtrinhdaotao/repository/thong_tin_chung_Repository.java
@@ -7,6 +7,7 @@ package com.mycompany.chuongtrinhdaotao.repository;
 import com.mycompany.chuongtrinhdaotao.model.thong_tin_chung;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,4 +17,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface thong_tin_chung_Repository extends JpaRepository<thong_tin_chung, Integer> {
    List<thong_tin_chung> findByTrangThai(int trangThai);
+   
+   @Query("SELECT t.id FROM thong_tin_chung t")
+   List<String> findAllId();
+
+   @Query("SELECT t.tenCTDT FROM thong_tin_chung t")
+   List<String> findAllTenCTDT();
+   
+   @Query("SELECT tt.id, tt.tenCTDT FROM thong_tin_chung tt")
+   List<Object[]> findAllIdAndTenCTDT();
 }
