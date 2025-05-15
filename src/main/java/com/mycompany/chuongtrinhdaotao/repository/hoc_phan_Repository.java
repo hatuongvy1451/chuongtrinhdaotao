b07f09ce1a0949a6baeedcbff2012ec12943dc1e
hatuongvy1451 <147377307+hatuongvy1451@users.noreply.github.com>
2025-05-15 10:34:02 +0700

    thống kê, in

--- src/main/java/com/mycompany/chuongtrinhdaotao/repository/hoc_phan_Repository.java
+++ src/main/java/com/mycompany/chuongtrinhdaotao/repository/hoc_phan_Repository.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.chuongtrinhdaotao.repository;
 
+import com.mycompany.chuongtrinhdaotao.model.HocPhanDTO;
 import com.mycompany.chuongtrinhdaotao.model.hoc_phan;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -37,4 +38,33 @@ public interface hoc_phan_Repository extends JpaRepository<hoc_phan, Integer>{
        "JOIN khung_chuong_trinh_kkt kkt ON kkt.idKhungCT.id = kct.id " +
        "WHERE kkt.idKhoiKT.id = :idKhoiKT")
     List<hoc_phan> getHocPhanByIdKhoiKienThuc(@Param("idKhoiKT") int idKhoiKT);
+    
+    @Query("""
+    SELECT new com.mycompany.chuongtrinhdaotao.model.HocPhanDTO(
+        hp.maHocPhan,
+        hp.tenHocPhan,
+        hp.soTinChi,
+        hp.soTietLyThuyet,
+        hp.soTietThucHanh,
+        hp.soTietThucTap,
+        hp.heSoHocPhan,
+        kct.maKhoi,
+        kct.tenKhoi
+    )
+    FROM hoc_phan hp
+    JOIN hp.khungChuongTrinh kct
+    JOIN kct.ctdt ctdt
+    WHERE ctdt.id = :ctdtId
+    ORDER BY kct.maKhoi ASC, hp.tenHocPhan ASC
+    """)
+     List<HocPhanDTO> findHocPhanByCTDTGroupedByKhoi(@Param("ctdtId") int ctdtId);
+
+
+     @Query("SELECT hp, kct.tenKhoi, khdh.hocKyThucHien " +
+             "FROM hoc_phan hp " +
+             "JOIN hp.khungChuongTrinh kct " +
+             "LEFT JOIN ke_hoach_day_hoc khdh ON khdh.hocPhan.id = hp.id " +
+             "WHERE kct.ctdt.id = :idCTDT " +
+             "ORDER BY kct.tenKhoi, hp.maHocPhan")
+     List<Object[]> findHocPhanTheoKhoiVoiHocKy(@Param("idCTDT") int idCTDT);
 }
