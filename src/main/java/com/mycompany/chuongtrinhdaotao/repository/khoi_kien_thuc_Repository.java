d53b3894ad9116278d22ca5b362bdc8caf7ba2c1
hatuongvy1451 <147377307+hatuongvy1451@users.noreply.github.com>
2025-05-15 15:02:36 +0700

    Update khoi_kien_thuc_Repository.java

--- src/main/java/com/mycompany/chuongtrinhdaotao/repository/khoi_kien_thuc_Repository.java
+++ src/main/java/com/mycompany/chuongtrinhdaotao/repository/khoi_kien_thuc_Repository.java
@@ -14,5 +14,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface khoi_kien_thuc_Repository extends JpaRepository<khoi_kien_thuc, Integer> {
-    
+    boolean existsByMaKhoi(String maKhoi);
 }
